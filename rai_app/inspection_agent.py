41b7165487228cff06f88d40ac36cb316e319633
Kajetan Rachwał <69043608+rachwalk@users.noreply.github.com>
2025-10-06 15:41:56 +0200

    feat: improve inspection
    Co-authored-by: maciejmajek <maciej.majek@robotec.ai>

--- rai_app/inspection_agent.py
+++ rai_app/inspection_agent.py
@@ -2,9 +2,13 @@ from __future__ import annotations
 
 import argparse
 import base64
+import queue
 import time
+from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
+from queue import Queue
+from threading import Thread
 from typing import Literal, cast
 
 import numpy as np
@@ -18,7 +22,13 @@ from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from llms import get_model
 from pydantic import BaseModel, Field
-from rai.communication.ros2 import ROS2Connector, ROS2Context, ROS2Message
+from rai.agents import BaseAgent
+from rai.communication.ros2 import (
+    ROS2Connector,
+    ROS2Context,
+    ROS2Message,
+    wait_for_ros2_topics,
+)
 from rai.messages import (
     HumanMultimodalMessage,
     preprocess_image,
@@ -28,18 +38,21 @@ from rosidl_runtime_py import message_to_ordereddict
 from sensor_msgs.msg import Image
 from tf2_ros import PoseStamped
 from tf_transformations import euler_from_quaternion
+from visualization_msgs.msg import Marker, MarkerArray
 
 from scripts.scene_manager import SceneManager
 
-SYSTEM_PROMPT = "You are an expert in warehouse environment based on AMR camera. You are tested in simulation."
+SYSTEM_PROMPT = "You are an expert in warehouse environment based on AMR camera. You are tested in simulation. You follow strict OSHA regulation: there should be no objects on the warehouse floor. Boxes directly under racks can be on the floor. Trash can be on the floor. Safety equipment can be on the floor. The OSHA guideline is for there to be no tripping hazard."
 # PROMPT = "Verify if there is an obstacle on a robot's path. Please don't report typical warehouse envirionemt as obstacles. To be an obstacle a object should be places in an unusual place and obstruct the clear navigation path of the robot. For example a package laying in the pathway might be an obstance and standing rack visible in the image is not."
-PROMPT = "Verify if there is an obstacle on warehouse alleys. Please report anomalies only. Please don't report typical warehouse envirionemt as obstacles."
+PROMPT = (
+    "Detect if there is an object on the floor. Decide if the object is trash or a box."
+)
 
 
 class AnomalyDescription(BaseModel):
     anomaly_detected: bool = Field(..., description="True if obstacle is detected")
-    obstacle_type: Literal["box", "contamination", "fallen_object", "other_object"] = (
-        Field(..., description="The type of the obstacle")
+    obstacle_type: Literal["box", "trash"] = Field(
+        ..., description="The type of the obstacle"
     )
     anomaly_description: str = Field(
         ...,
@@ -105,7 +118,16 @@ def are_anomalies_close(
     return poses_match and obstacle_type_match
 
 
-class VlmWarehouseInspector:
+@dataclass
+class InspectionTask:
+    b64_img: str
+    image: np.ndarray
+    img_stamp: rclpy.time.Time
+    robot_location_stamp: rclpy.time.Time
+    object_pose: Pose
+
+
+class VlmWarehouseInspector(BaseAgent):
     def __init__(
         self,
         vlm: ChatOllama | ChatOpenAI,
@@ -120,6 +142,8 @@ class VlmWarehouseInspector:
         system_prompt: str = SYSTEM_PROMPT,
         match_anomaly_max_distance: float = 0.1,
         match_anomaly_max_yaw_degrees: float = 10.0,
+        n_seconds: int = 1,
+        debug: bool = True,
     ):
         self.camera_topic = camera_topic
         self.ego_target_frame = ego_target_frame
@@ -130,7 +154,7 @@ class VlmWarehouseInspector:
         self.system_prompt = system_prompt
         self.match_anomaly_max_distance = match_anomaly_max_distance
         self.match_anomaly_max_yaw_degrees = match_anomaly_max_yaw_degrees
-
+        self.debug = debug
         self.connector = ROS2Connector(executor_type="multi_threaded")
         self.get_logger = self.connector.node.get_logger
         self.scene_manager = SceneManager(
@@ -140,11 +164,20 @@ class VlmWarehouseInspector:
         )
 
         self.vlm = vlm.with_structured_output(AnomalyDescription)
+        self.n_seconds = n_seconds
 
         self.reported_anomalies: list[Anomaly] = list()
+        wait_for_ros2_topics(self.connector, [self.camera_topic])
+        # debug
+        self.marker_array = MarkerArray()
+        self.process_queue: Queue = Queue()
+        self.last_processed = time.time()
+        self.running = True
+        self.vlm_thread = Thread(target=self.vlm_worker)
 
-    def loop(self):
-        while True:
+    def run(self):
+        self.vlm_thread.start()
+        while self.running:
             try:
                 msg: Image = self.connector.receive_message(
                     self.camera_topic, timeout_sec=1.0
@@ -154,15 +187,29 @@ class VlmWarehouseInspector:
                     f"Failed to receive a message from {self.camera_topic}"
                 )
                 continue
-            stamp = rclpy.time.Time.from_msg(msg.header.stamp)
+            img_stamp = rclpy.time.Time.from_msg(msg.header.stamp)
             image = CvBridge().imgmsg_to_cv2(  # type: ignore
                 msg, desired_encoding="bgr8"
             )
-            b64_img = preprocess_image(image)
+            robot_location = self.get_robot_location()
+            robot_location_stamp = rclpy.time.Time.from_msg(robot_location.header.stamp)
 
-            ts = time.perf_counter()
-            self.vlm_callback(b64_img, image, stamp)
-            self.get_logger().info(f"VLM analysis took: {time.perf_counter() - ts}")
+            try:
+                _, candidate_pose = self.scene_manager.get_trash_pose(
+                    robot_location.pose, [x.pose for x in self.reported_anomalies]
+                )
+            except ValueError:
+                self.get_logger().debug("No candidate pose detected")
+                candidate_pose = None
+
+            if candidate_pose:
+                self.get_logger().debug(f"Candidate pose: {candidate_pose}")
+
+                b64_img = preprocess_image(image)
+                task = InspectionTask(
+                    b64_img, image, img_stamp, robot_location_stamp, candidate_pose
+                )
+                self.process_queue.put(task)
 
     def check_if_anomaly_is_reported(self, anomaly: Anomaly) -> bool:
         for reported_anomaly in self.reported_anomalies:
@@ -175,51 +222,73 @@ class VlmWarehouseInspector:
                 return True
         return False
 
-    def vlm_callback(self, b64_img: str, image: np.ndarray, stamp: rclpy.time.Time):
-        robot_location = self.get_robot_location()
-        result: AnomalyDescription = self.detect_obstacle(b64_img)
-        self.get_logger().info(f"Result: {result.model_dump()}")
+    def vlm_worker(self):
+        self.last_processed = time.time()
+        while self.running:
+            try:
+                task = self.process_queue.get(timeout=1)
+                current = time.time()
+                if current - self.last_processed < self.n_seconds:
+                    continue
 
-        img_stamp = stamp.nanoseconds
-        robot_location_stamp = rclpy.time.Time.from_msg(
-            robot_location.header.stamp
-        ).nanoseconds
-        time_diff = abs(img_stamp - robot_location_stamp) / 1e9
-        if time_diff > 0.5:
-            self.get_logger().error(
-                f"Time difference between image and robot location is too large: {time_diff:.3f} seconds"
-            )
-            return
+            except queue.Empty:
+                continue
+
+            ts = time.perf_counter()
+            self.vlm_process(task)
+            self.last_processed = time.time()
+            self.get_logger().info(f"VLM analysis took: {time.perf_counter() - ts}")
+
+    def vlm_process(self, task: InspectionTask):
+        result: AnomalyDescription = self.detect_obstacle(task.b64_img)
+        print("#############")
+        self.get_logger().info(f"Result: {result.model_dump()}")
+        print("#############")
 
         if result.anomaly_detected:
-            self.get_logger().info(f"Obstacle detected: {result.model_dump()}")
-            try:
-                _, trash_pose = self.scene_manager.get_trash_pose(robot_location.pose)
-            except ValueError:
-                self.get_logger().error("No trash pose detected")
-                return
-
-            self.get_logger().info(f"Trash pose: {trash_pose}")
-
             message = Anomaly()
-            message.pose = trash_pose
+            message.pose = task.object_pose
             message.obstacle_type = result.obstacle_type
             message.anomaly_description = result.anomaly_description
+            self.get_logger().info(f"Sending anomaly: {message}")
 
             if self.check_if_anomaly_is_reported(message):
                 self.get_logger().info(f"Anomaly already reported: {message}")
                 return
 
-            filename = save_image_to_disk(b64_img, self.anomaly_images_dir)
+            filename = save_image_to_disk(task.b64_img, self.anomaly_images_dir)
             message.filename = filename
 
             self.reported_anomalies.append(message)
 
+            if self.debug:
+                self._publish_marker(message)
             self.connector.send_message(
                 ROS2Message(payload=message_to_ordereddict(message)),
                 target=self.anomalies_topic,
                 msg_type="robotec_kairos_ur10/msg/Anomaly",
             )
+
+    def _publish_marker(self, message: Anomaly):
+        # publish marker to Rviz 2
+        marker = Marker()
+        marker.header.frame_id = self.ego_target_frame
+        marker.type = Marker.CUBE
+        marker.id = len(self.marker_array.markers)
+        marker.pose = message.pose
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0
+        self.marker_array.markers.append(marker)
+        self.connector.send_message(
+            ROS2Message(payload=message_to_ordereddict(self.marker_array)),
+            target="/marker",
+            msg_type="visualization_msgs/msg/MarkerArray",
+        )
 
     def detect_obstacle(self, b64_img: str) -> AnomalyDescription:
         task = [
@@ -264,6 +333,10 @@ class VlmWarehouseInspector:
         pose.pose.orientation = transform.transform.rotation
         return pose
 
+    def stop(self):
+        self.running = False
+        self.vlm_thread.join()
+
 
 @ROS2Context()
 def main():
@@ -272,16 +345,21 @@ def main():
     parser.add_argument(
         "--spawnables-file", type=str, default="scripts/resources/spawnables.csv"
     )
-    parser.add_argument("--vlm-vendor", type=str)
-    parser.add_argument("--vlm-model", type=str)
-    parser.add_argument("--vlm-base_url", type=str, default=None)
+    parser.add_argument("--vlm-vendor", type=str, default="ollama")
+    parser.add_argument("--vlm-model", type=str, default="gemma3:27b")
+    parser.add_argument(
+        "--vlm-base_url",
+        type=str,
+        default="http://via-ip-robo-srv-004.robotec.tm.pl:11434",
+    )
     parser.add_argument(
         "--camera-topic", type=str, default="/rgbd_camera/camera_image_color"
     )
-    parser.add_argument("--ego-source-frame", type=str, default="odom")
-    parser.add_argument("--ego-target-frame", type=str, default="egobase_footprint")
+    parser.add_argument("--ego-source-frame", type=str, default="egobase_footprint")
+    parser.add_argument("--ego-target-frame", type=str, default="odom")
     parser.add_argument("--anomaly-images-dir", type=str, default="./anomaly_images")
     parser.add_argument("--anomalies-topic", type=str, default="/inspection_result")
+    parser.add_argument("--n-seconds", type=int, default=5)
     args = parser.parse_args()
     vlm = get_model(
         model=args.vlm_model, vendor=args.vlm_vendor, base_url=args.vlm_base_url
@@ -296,9 +374,10 @@ def main():
         ego_source_frame=args.ego_source_frame,
         anomaly_images_dir=args.anomaly_images_dir,
         anomalies_topic=args.anomalies_topic,
+        n_seconds=args.n_seconds,
     )
 
-    inspector.loop()
+    inspector.run()
 
 
 if __name__ == "__main__":
