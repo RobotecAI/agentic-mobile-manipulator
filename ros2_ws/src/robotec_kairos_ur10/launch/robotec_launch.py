8d9934c441a94e5023f8e0cdd62a9aa9a6cd7bca
Kacper Dąbrowski <kacper.dabrowski@robotec.ai>
2025-10-07 18:31:05 +0200

    feat: add pointcloud to nav2 sources
    Signed-off-by: Kacper Dąbrowski <kacper.dabrowski@robotec.ai>
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    Co-authored-by: Michał Pełka <michal.pelka@robotec.ai>

--- ros2_ws/src/robotec_kairos_ur10/launch/robotec_launch.py
+++ ros2_ws/src/robotec_kairos_ur10/launch/robotec_launch.py
@@ -131,10 +131,36 @@ def launch_setup(context, *args, **kwargs):
         ]
     )
 
+    rgbd_pc = ComposableNodeContainer(
+            name='container0',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='depth_image_proc',
+                    plugin='depth_image_proc::PointCloudXyzrgbNode',
+                    name='point_cloud_xyzrgb_node',
+                    remappings=[('rgb/camera_info', (robot_namespace, '/rgbd_camera/camera_info')),
+                                ('rgb/image_rect_color', (robot_namespace, '/rgbd_camera/camera_image_color')),
+                                ('depth_registered/image_rect', (robot_namespace, '/rgbd_camera/camera_image_depth')),
+                                ('/points', (robot_namespace, '/rgbd_camera/pointcloud'))],
+                    parameters=[
+                        {'use_sim_time': True},
+                        {'approximate_sync': True}
+                    ]
+                ),
+            ],
+            output='screen',
+            parameters=[{'use_sim_time': True, 'approximate_sync': True}]
+    
+    )
+
     return [
         run_move_group_node, 
         rviz_node, 
-        marker_server
+        marker_server,
+        rgbd_pc
     ]
 
 
