7fdfe90f94693e0ecbb693e7cb11bced17408bb0
Bartek Boczek <bartlomiej.boczek@robotec.ai>
2025-10-08 15:35:30 +0200

    feat: improve fill slots script (#248)

--- scripts/fill_slots.py
+++ scripts/fill_slots.py
@@ -1,33 +1,41 @@
-from rai.communication.ros2 import ROS2Connector, ROS2Context
-from scene_manager import SceneManager
+import random
+
+from rai.communication.ros2 import ROS2Connector
+
+from scripts.scene_manager import SceneManager
 
 
-@ROS2Context()
-def main(debug: bool = False, namespace=""):
-    connector = ROS2Connector(
-        executor_type="multi_threaded", node_name="ground_truth_manipulation"
-    )
+def main(
+    connector=None,
+    clear=True,
+    entity_types: list[str] | None = None,
+    skip_slots: list[str] | None = None,
+    fraction_of_slots: int | None = None,
+):
+    """
+    Fill slots with entities
+
+    Args:
+        connector (ROS2Connector, optional): ROS2 connector. Defaults to None.
+        clear (bool, optional): Clear the scene. Defaults to True.
+        entity_types (list[str] | None, optional): List with entity types. Defaults to None.
+        skip_slots (list[str] | None, optional): List with slot names to skip. Exact names can be passed or substrings of names. E.g. ["H01/RackSlot1", "H02", "t2"]. Defaults to None.
+        fraction_of_slots (int | None, optional): Use 1/fraction_of_slots for spawning. Defaults to None.
+    """
+    if connector is None:
+        connector = ROS2Connector(
+            executor_type="multi_threaded", node_name="ground_truth_manipulation"
+        )
     scene_manager = SceneManager(
         slots_file="scripts/resources/slots.csv",
         spawnables_file="scripts/resources/spawnables.csv",
         connector=connector,
     )
-    scene_manager.clear_scene()
+    if clear:
+        scene_manager.clear_scene()
 
-    entity_types = [
-        "cardboardbox01_v01",
-        "cardboardbox01_v02D",
-        "cardboardbox01_v03",
-        "cardboardbox02_v01",
-        "cardboardbox02_v02D",
-        "cardboardbox03_v01",
-        "cardboardbox03_v02O",
-        "cardboardbox04_v01",
-        "cardboardbox05_v01",
-        "cardboardbox06_v01",
-        "cardboardbox07_v01",
-        "cardboardbox08_v01",
-    ]
+    if entity_types is None:
+        entity_types = [item for item in scene_manager.spawnable_to_uri]
 
     all_slot_names = []
 
@@ -35,24 +43,40 @@ def main(debug: bool = False, namespace=""):
         scene_manager.find_empty_slots_on_racks()
         | scene_manager.find_empty_slots_on_tables()
     )
+
+    # Select 1/x of all slots randomly
+    if fraction_of_slots:
+        empty_slots = {
+            rack: random.sample(slots, max(1, len(slots) // fraction_of_slots))
+            for rack, slots in empty_slots.items()
+        }
+
     for rack in empty_slots:
         for slot in empty_slots[rack]:
-            all_slot_names.append(f"{slot}")
+            all_slot_names.append(slot.tag)
+
+    if skip_slots is not None:
+        all_slot_names = [
+            slot for slot in all_slot_names if all(s not in slot for s in skip_slots)
+        ]
+
     spawn_slot_names = all_slot_names
     spawn_entity_types = [
         entity_types[i % len(entity_types)] for i in range(len(spawn_slot_names))
     ]
 
     scene_manager.populate_scene(
-        spawn_slot_names, spawn_entity_types, std_xy=0.05, std_yaw=0.05
+        spawn_slot_names, spawn_entity_types, std_xy=0.05, std_yaw=0.08
     )
 
 
 if __name__ == "__main__":
     import argparse
 
+    import rclpy
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--namespace", type=str, default="")
+    parser.add_argument("--clear", action="store_true")
     args = parser.parse_args()
+    rclpy.init()
     main(**vars(args))
