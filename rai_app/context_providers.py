0af4679f8fa14462ca4b0d860f8508d21b7c075b
Jakub Matejczyk <58983084+jmatejcz@users.noreply.github.com>
2025-10-17 17:15:34 +0200

    feat: orchestrator api + high prio queue (#295)
    Co-authored-by: Kajetan Rachwał <69043608+rachwalk@users.noreply.github.com>

--- rai_app/context_providers.py
+++ rai_app/context_providers.py
@@ -10,6 +10,10 @@ class WarehouseContext(ContextProvider):
         self.scene_manager = scene_manager
 
     def get_context(self) -> str:
-        context = "Collection t2 is the outbound shipment table used for preparing shipments.\nCollection t4 is the inspection table used for inspecting returned packages by a human.\n"
+        context = (
+            "Collection t2 is the outbound shipment table used for preparing shipments."
+            "\nCollection t4 is the inspection table used for inspecting returned packages by a human.\n"
+            " Tables t1 and t3 have no special purpose, but are available."
+        )
         context += self.scene_manager.get_warehouse_collections_description()
         return context
