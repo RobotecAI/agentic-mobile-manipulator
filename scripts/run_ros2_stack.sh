88be7d162939ed4d04cabf67ae1946030af22c1c
Kajetan Rachwał <69043608+rachwalk@users.noreply.github.com>
2025-10-24 17:55:24 +0200

    feat: update run scripts

--- scripts/run_ros2_stack.sh
+++ scripts/run_ros2_stack.sh
@@ -1,6 +1,9 @@
 (
   ros2 launch robotec_kairos_ur10 robotec_launch.py &
+  ros2 run mobile_manipulator_hmi utilization_node &
   uv run python rai_app/agents/nav2_agent.py $1 &
   uv run python rai_app/agents/moveit2_agent.py $1 &
+  uv run python rai_app/environment/scene_agent.py &
+  uv run python rai_app/control/nav_lifecycle_node.py &
   wait
 )
