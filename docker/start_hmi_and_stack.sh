adb28ca6d788eca5c30b3b854bc599a57c8fa7c5
Bartek Boczek <bartlomiej.boczek@robotec.ai>
2026-01-13 17:22:52 +0100

    feat(`docker`): each ros2 stack node is a separate docker compose service (#7)
    * feat(`docker`): each ros2 stack node is a separate docker compose service
    
    * fix: scene agent path
    
    * refactor(`docker`): `compose.amd.yaml` config
    
    * refactor(`docker`): adapt amd compose file
    
    ---------
    
    Co-authored-by: jmatejcz <jakub.matejczyk@robotec.ai>

--- docker/start_hmi_and_stack.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-source /root/MobileManipulatorDemo/ros2_ws/install/setup.bash
-
-exec ros2 launch mobile_manipulator_hmi hmi_launch.py & /root/MobileManipulatorDemo/run_ros2_stack.sh
--- docker/start_orchestrator.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-source /root/MobileManipulatorDemo/ros2_ws/install/setup.bash
-
-exec uv run python rai_app/agents/agent_orchestrator.py
