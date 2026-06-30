2f07dc9f7a014892023e439eb207ff2e28764559
Kacper Dąbrowski <kacper.dabrowski@robotec.ai>
2026-06-30 16:03:44 +0200

    Catch moveit errors in housekeep tool
    Signed-off-by: Kacper Dąbrowski <kacper.dabrowski@robotec.ai>

--- rai_app/agents/tools.py
+++ rai_app/agents/tools.py
@@ -887,6 +887,8 @@ class HouseKeepTool(WarehouseTool):
                     pos, collection_name=rack, approach_distance=self.approach_distance
                 )
             except RuntimeError as e:
+                if "arm" in str(e): # don't count moveit related exceptions as "side not available"
+                    raise e
                 sides_not_available += 1
                 logging.warning(e)
             except Exception as e:
